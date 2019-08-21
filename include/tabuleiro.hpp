cc88990da91a6d276beb8b22bbc9d575a5647963
Nathalia <nathalyabezerra@live.com>
2019-08-20 21:47:55 -0300

    tabuleiro.hpp update

--- include/tabuleiro.hpp
+++ include/tabuleiro.hpp
@@ -5,12 +5,10 @@
 #include <stdlib.h>
 #include <iostream>
 
-void imprimirTabuleiro(char **tabuleiro, int linhas, int colunas, int preg); 
-int completo(char **tabuleiro, int linhas, int colunas);
-void limpar_tabuleiro(char **tabuleiro, int linhas, int colunas);
+void imprimirTabuleiro(char **tabuleiro, int linhas, int colunas, int preg); //IMPRIME O TABULEIRO 	
+int completo(char **tabuleiro, int linhas, int colunas);	//VERIFICA SE O TABULEIRO ESTÁ COMPLETO OU NÃO
+void limpar_tabuleiro(char **tabuleiro, int linhas, int colunas);	//ZERA O TABULEIRO.
+void jogada(char **tabuleiro, int linhas, int colunas, char j, int jj, int *preg); //POSICIONA X E O E IDENTIFICA SE A POSIÇÃO JÁ ESTÁ OCUPADA
 
-
-//O módulo Tabuleiro deve ser responsável por criar o tabuleiro, zerar o tabuleiro, 
-//verificar se o tabuleiro está completo, posicionar os X e 0 e verificar se a casa do tabuleiro já se encontra ocupada.
 
 #endif
