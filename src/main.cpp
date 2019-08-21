d9381cb9e82b87437d0378ad9ade933efa382e84
Nathalia <nathalyabezerra@live.com>
2019-08-20 21:48:43 -0300

    main.cpp update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "jogo.hpp"
 #include "jogador.hpp"
 #include "tabuleiro.hpp"
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -10,17 +12,17 @@ char **tabuleiro; //Declaração da matriz tabuleiro
 char j1, j2; //Jogador 1 e Jogador 2;
 int preg = 0, jj1 = 1, jj2 = 2; /*Variáveis auxiliares. Contador para fazer as jogadas, preg para auxiliar a impressão do tabuleiro pré-game
 										jj1 e jj2 para serem os equivalentes inteiros [1 e 2] dos jogadores.*/ 
-int i, j, linhas = 3, colunas = 3; 
+int i, j, linhas = 3, colunas = 3; /*Variáveis auxiliares para os fors, o número de linhas e colunas.*/ 
 char win;  
 
 tabuleiro = (char**)calloc(linhas, sizeof(char*));	//Alocando o tabuleiro
 
 for(i = 0; i<linhas; i++){
-	tabuleiro[i] = (char*)calloc(colunas, sizeof(char));		//Alocando o tabuleiro
+	tabuleiro[i] = (char*)calloc(colunas, sizeof(char));	//Alocando o tabuleiro
 }
 
 for(i = 0; i<linhas; i++){
-	for(j = 0; j<colunas; j++){
+	for(j = 0; j<colunas; j++){			/*Definindo um caracter padrão para inicializar o tabuleiro, neste caso, espaço*/ 
 		tabuleiro[i][j] = ' '; 
 	}
 }
@@ -28,15 +30,15 @@ for(i = 0; i<linhas; i++){
 //menu();
 int resp;
 do{
-imprimirTabuleiro(tabuleiro, linhas, colunas, preg); //Imprime tabuleiro antes do jogo começar
+imprimirTabuleiro(tabuleiro, linhas, colunas, preg); //Imprime tabuleiro antes do jogo começar para referencias de posiçoes
 
-limpar_tabuleiro(tabuleiro, linhas, colunas);	//Zera o tabuleiro
+limpar_tabuleiro(tabuleiro, linhas, colunas);	//Zera o tabuleiro, torna tudo zero novamente
 
-escolherJogador(&j1, &j2);
-imprimirTabuleiro(tabuleiro, linhas, colunas, preg);
-controla_rodadas(tabuleiro, linhas, colunas, preg, j1, j2, jj1, jj2, win);
+escolherJogador(&j1, &j2); //Função para o jogador escolher qual será X e qual será O
 
-cout<<"\nDeseja jogar novamente? 1. Sim 2. Não\n";
+controla_rodadas(tabuleiro, linhas, colunas, preg, j1, j2, jj1, jj2, win); //Controla as rodadas, quem joga na hora etc. 
+
+cout<<"\nDeseja jogar novamente? 1. Sim 2. Não\n";	//APós o primeiro jogo ocorrer, pergunta se o usuário quer jogar novamente.
 cin >> resp; 
 if(resp == 1){
 	limpar_tabuleiro(tabuleiro, linhas, colunas);
@@ -44,8 +46,9 @@ if(resp == 1){
 }else{
 	cout<<"\nTudo bem, até a próxima! :)\n ";
 }
-}while(resp < 2 && resp != 0);
+}while(resp < 2 && resp != 0); //O laço é encerrado se a resposta do usuário for que não deseja jogar novamente. 
 
+free(tabuleiro);
 
 return 0;
 
