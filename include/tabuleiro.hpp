d7c45640585b38ee0aac3c1392ea9e133e970d99
Nathalia <nathalyabezerra@live.com>
2019-08-21 01:54:38 -0300

    up tabuleiro.hpp

--- include/tabuleiro.hpp
+++ include/tabuleiro.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 void imprimirTabuleiro(char **tabuleiro, int linhas, int colunas, int preg); //IMPRIME O TABULEIRO 	
-int completo(char **tabuleiro, int linhas, int colunas);	//VERIFICA SE O TABULEIRO ESTÁ COMPLETO OU NÃO
+void completo(char **tabuleiro, int linhas, int colunas);	//VERIFICA SE O TABULEIRO ESTÁ COMPLETO OU NÃO
 void limpar_tabuleiro(char **tabuleiro, int linhas, int colunas);	//ZERA O TABULEIRO.
 void jogada(char **tabuleiro, int linhas, int colunas, char j, int jj, int *preg); //POSICIONA X E O E IDENTIFICA SE A POSIÇÃO JÁ ESTÁ OCUPADA
 
