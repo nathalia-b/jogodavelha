73d56bf67189cd6a1fccef5bbdf8debf12a00d79
Nathalia <nathalyabezerra@live.com>
2019-08-20 21:47:31 -0300

    jogo.hpp update

--- include/jogo.hpp
+++ include/jogo.hpp
@@ -6,11 +6,10 @@
 #include <iostream>
 //O módulo Jogo deve iniciar o jogo, escolher os jogadores, controla as rodadas, verificar se houve ganhador.
 
-void escolherJogador(char *j1, char *j2);
-void jogada(char **tabuleiro, int linhas, int colunas, char j, int jj, int *preg); 
-int verificando_vencedor(char **t, char j1, char j2, char *win);
-void ganhou(char j, int jj, int jjj);
-void controla_rodadas(char **tabuleiro, int linhas, int colunas, int preg, char j1, char j2, int jj1, int jj2, char win);
+void escolherJogador(char *j1, char *j2); //O JOGADOR ESCOLHE SE FICARÁ COM X OU O
+int verificando_vencedor(char **t, char j1, char j2, char *win); //VERIFICA SE HÁ UM VENCEDOR
+void ganhou(char j, int jj, int jjj);	//IMPRIME AVISO PARA DIZER QUE O JOGADOR Y GANHOU
+void controla_rodadas(char **tabuleiro, int linhas, int colunas, int preg, char j1, char j2, int jj1, int jj2, char win); //CONTROLA AS RODADAS [QUEM JOGA PRIMEIRO, QUEM JOGA DEPOIS, ETC]
 
 
 #endif
