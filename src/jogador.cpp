f1af4f1a587948aec22079b0e0adcd47f9b4e569
Nathalia <nathalyabezerra@live.com>
2019-08-27 09:16:12 -0300

    jogador.cpp up

--- src/jogador.cpp
+++ src/jogador.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 
 void aviso(int jj){  
-cout<<"\n > Jogador " << jj << ", é sua vez! Escolha a linha e a coluna de sua jogada (de 0 a 2 apenas)\n";
+cout<<"\n -> Jogador " << jj << ", é sua vez! Escolha a linha e a coluna de sua jogada (de 0 a 2 apenas)\n";
 }
 
 
 void status(char **tabuleiro, int linhas, int aux, int colunas, char jx){
 int i, j;
 	if(aux == 1){
-		cout<<"\tFique atento, J1! Atualmente você ocupa as seguintes posições: ";
+		cout<<"\tFique atento, J1! Atualmente você ocupa as seguintes posições:\n";
 	}else{
-		cout<<"\tFique atento, J2! Atualmente você ocupa as seguintes posições: ";
+		cout<<"\tFique atento, J2! Atualmente você ocupa as seguintes posições:\n";
 	}
 
 	for(i = 0; i<linhas; i++){
@@ -35,7 +35,7 @@ cout<<"\n";
 void lembrete(int jj, char j, char **tabuleiro, int linhas, int colunas, int preg){
 int resp, x = (linhas*colunas)-preg;
 cout<<"\n\tJogador " << jj << ", lembre-se: você é o '"<<j<<"'!\n";
-cout<<"\tFaltam "<<x<<" rodadas para o fim do jogo. Deseja ver como está o tabuleiro nesse momento? 1. Sim 2. Não\n";
+cout<<"\tFaltam "<<x<<" rodadas para o fim do jogo.\nDeseja ver como está o tabuleiro nesse momento? 1. Sim 2. Não\n";
 cout<<"\t";
 cin>>resp;
 if(resp == 1){
