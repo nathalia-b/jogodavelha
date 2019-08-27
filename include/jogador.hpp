2e45cc68f92c28547aea115467fb719eba1057c6
Nathalia <nathalyabezerra@live.com>
2019-08-27 10:29:34 -0300

    jogador.hpp up

--- include/jogador.hpp
+++ include/jogador.hpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void aviso(int jj); //IMPRIME AVISO PARA DIZER QUE É A VEZ DO JOGADOR;
 void status(char **tabuleiro, int linhas, int aux, int colunas, char jx); /*INFORMA QUAIS POSIÇÕES O JOGADOR OCUPA ATUALMENTE SE ELE TENTAR PREENCHER UMA POSIÇÃO JÁ OCUPADA*/
-void lembrete(int jj, char j, char **tabuleiro, int linhas, int colunas, int preg); /* INFORMA QUANTAS RODADAS FALTAM PRA ACABAR O JOGO E PERGUNTA SE QUER VER O TABULEIRO ATUAL*/ 
+void lembrete(int jj, char j, char **tabuleiro, int linhas, int colunas, int preg); /* INFORMA QUANTAS RODADAS FALTAM PRA ACABAR O JOGO*/ 
 
 
 #endif
